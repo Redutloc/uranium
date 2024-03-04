0b856069063ccd30f12016c306e25c09dcf1d902
Redutloc <160947063+Redutloc@users.noreply.github.com>
2024-03-04 17:59:21 +0300

    Add files via upload

---
+++ HOMEATT/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+//Задача 3
+public class Example
+{
+    public static void reverseList<T>(List<T> list)
+    {
+
+        if (list == null || list.Count <= 1)
+        {
+            return;
+        }
+
+
+        T value = list[0];
+        list.RemoveAt(0);
+
+
+        reverseList(list);
+
+        list.Add(value);
+    }
+
+    public static void Main()
+    {
+        List<int> list = new List<int> { 2, 4, 6, 8 };
+
+        reverseList(list);
+        Console.WriteLine(String.Join(',', list));
+    }
+}
